8ca9e1170579c0fa01bfc0c7e8f8e5644d822e7c
DaiNaka1207 <77893229+DaiNaka1207@users.noreply.github.com>
2021-10-31 01:16:14 +0900

    Create MVC

---
+++ database/migrations/2021_10_31_005341_create_chats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('user_name', 20)->default('noname');
+            $table->string('user_identifier', 20);
+            $table->string('message', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
