a277f9d2634efb84ef68acac283708ade59f602d
DaiNaka1207 <77893229+DaiNaka1207@users.noreply.github.com>
2021-11-10 23:49:50 +0900

    Add User_name session & autofocus

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -19,6 +19,9 @@ class ChatController extends Controller
         // ユーザーIDをセッションに登録
         $user_identifier = $request->session()->get('user_identifier', Str::random(20));
         session(['user_identifier' => $user_identifier]);
+
+        // ユーザー名を変数に登録（デフォルト値：Guest）
+        $user_name = $request->session()->get('user_name', 'Guest');
         
         // データーベースの件数を取得
         $length = Chat::all()->count();
@@ -27,7 +30,7 @@ class ChatController extends Controller
         $display = 5;
 
         $chats = Chat::offset($length-$display)->limit($display)->get();
-        return view('chat/index',compact('chats','user_identifier'));
+        return view('chat/index',compact('chats', 'user_identifier', 'user_name'));
     }
 
     /**
@@ -48,6 +51,9 @@ class ChatController extends Controller
      */
     public function store(Request $request)
     {
+        // ユーザー名をフォームから取得してセッションに登録
+        session(['user_name' => $request->user_name]);
+
         $chat = new Chat;
         $form = $request->all();
         $chat->fill($form)->save();
--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -49,8 +49,8 @@
         <form class="my-4 py-2 px-4 rounded-lg bg-gray-300 text-sm flex flex-col md:flex-row flex-grow" action="/chat" method="POST">
             @csrf
             <input type="hidden" name="user_identifier" value={{$user_identifier}}>
-            <input class="py-1 px-2 rounded text-center flex-initial" type="text" name="user_name" placeholder="UserName" maxlength="20">
-            <input class="mt-2 md:mt-0 md:ml-2 py-1 px-2 rounded flex-auto" type="text" name="message" placeholder="Input message." maxlength="200">
+            <input class="py-1 px-2 rounded text-center flex-initial" type="text" name="user_name" placeholder="UserName" maxlength="20" value="{{$user_name}}">
+            <input class="mt-2 md:mt-0 md:ml-2 py-1 px-2 rounded flex-auto" type="text" name="message" placeholder="Input message." maxlength="200" autofocus>
             <button class="mt-2 md:mt-0 md:ml-2 py-1 px-2 rounded text-center bg-gray-500 text-white" type="submit">Send</button>
         </form>
     </body>
